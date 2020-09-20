1267fd6202a29dd0df7cd3bad31e75921e6558d6
Agustin Miura <agustin.miura@gmail.com>
2020-09-19 23:49:06 -0300

    Created Twitter producer example

--- kafka-beginner-course/src/main/java/ar/com/miura/fullexample/TwitterProducer.java
+++ kafka-beginner-course/src/main/java/ar/com/miura/fullexample/TwitterProducer.java
@@ -1,32 +1,45 @@
 package ar.com.miura.fullexample;
 
+import ar.com.miura.Utils;
+import ar.com.miura.kakfa.tutorial1.ConsumerDemo;
 import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.event.Event;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterProducer.class.getName());
+
     public static void main(String[] args) {
-        new TwitterProducer().run();
+        try {
+            new TwitterProducer().run();
+        } catch (IOException e) {
+            LOGGER.error(" Error ", e);
+        }
     }
 
-    public TwitterProducer() {
-
-
+    public TwitterProducer() throws IOException {
     }
 
-    public void createTwitterClient() {
-        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+    public Client createTwitterClient(BlockingQueue<String> msgQueue) throws IOException  {
+
         BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
 
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
@@ -34,14 +47,43 @@ public class TwitterProducer {
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
 
         List<Long> followings = Lists.newArrayList(1234L, 566788L);
-        List<String> terms = Lists.newArrayList("kakfa");
+        List<String> terms = Lists.newArrayList("bitcoin");
         hosebirdEndpoint.trackTerms(terms);
 
         // These secrets should be read from a config file
-        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+        Properties properties = Utils.readPropertiesFile("twitter.properties", this.getClass());
+        Authentication hosebirdAuth = new OAuth1(properties.getProperty("api.key"), properties.getProperty("api.secret"), properties.getProperty("api.token"), properties.getProperty("api.token.secret"));
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
     }
 
-    public void run() {
+    public void run() throws IOException {
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
 
+        Client client = createTwitterClient(msgQueue);
+        client.connect();
+
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                LOGGER.error(" Error thread interrupted ", e);
+                client.stop();
+            }
+            if (msg != null) {
+                LOGGER.info(" The message is {} ", msg);
+            }
+        }
+        LOGGER.info(" End of application ");
     }
 }
